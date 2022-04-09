88c59bbd9acdb8582cc6072ab4800f1b9bd07b07
JobTracking <tuncalin@outlook.com>
2022-04-09 12:29:15 +0300

    .

--- RabbitMQ.WatermarkEkleme/Startup.cs
+++ RabbitMQ.WatermarkEkleme/Startup.cs
@@ -28,7 +28,7 @@ namespace RabbitMQ.WatermarkEkleme
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(Sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")) });
+            services.AddSingleton(Sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")),DispatchConsumersAsync=true });
             services.AddSingleton<RabbitMQPublisher>();
             services.AddSingleton<RabbitMQClientService>();
 
